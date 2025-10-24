89949b3c645c766e8a0ef101b2bafee7ae045fa4
najmuddinmuizz@gmail.com <midorishouto@gmail.com>
2025-10-24 23:29:26 +0800

    Initial commit - NimbusIQ project files

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+  const firebaseConfig = {
+    apiKey: "AIzaSyD4BMltzx0XhbJTFL6mJi55Hh7mQa4Cee8",
+    authDomain: "air-with-cloud-monitoring.firebaseapp.com",
+    databaseURL: "https://air-with-cloud-monitoring-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "air-with-cloud-monitoring",
+    storageBucket: "air-with-cloud-monitoring.appspot.com",
+    messagingSenderId: "365608968348",
+    appId: "1:365608968348:web:e5f9f4d679a2bac8468d59",
+    measurementId: "G-DJ2Z30Q9KY"
+  };
